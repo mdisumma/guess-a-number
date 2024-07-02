160b42cc1f2612b0a2a6349d7990e6a79131804b
Marco Di Summa <mdisumma@yahoo.com>
2024-07-02 20:53:27 +0100

    initial version

--- script.js
+++ script.js
@@ -1,17 +1,39 @@
-const result = 3;
-console.log(typeof number);
+const result = Math.round(Math.random() * 100);
+console.log(result);
 
 const input = document.querySelector("#input");
-const button = document.querySelector("#button");
+const submit = document.querySelector("#submit");
+const closerLabel = document.querySelector(".answere");
+const answere = document.querySelector("#answere");
+const previousLabel = document.querySelector(".previous");
+const previous = document.querySelector("#previous");
 
-console.log(button);
-console.log(input);
+let guess = result;
+let counter = guess - result;
 
-button.addEventListener("click", function () {
-  let guess = Number(input.value);
-  if (guess === result) {
-    console.log("yes");
-  } else {
-    console.log("no");
+submit.addEventListener("click", function () {
+  const guess = Number(input.value);
+
+  if (guess - result === 0) {
+    console.log("you won");
+    closed.textContent = "you won";
+    closed.style = "color:red";
+  }
+  if (guess - result !== 0) {
+    let preCounter = counter;
+    let numberBefore = guess;
+    console.log("counter before " + counter);
+    counter = Math.abs(guess - result);
+    console.log("pre counter " + preCounter);
+    console.log("counter after " + counter);
+
+    if (preCounter < counter) {
+      console.log("no");
+      answere.textContent = "no";
+    }
+    if (preCounter > counter) {
+      console.log("yes");
+      answere.textContent = "yes";
+    }
   }
 });
